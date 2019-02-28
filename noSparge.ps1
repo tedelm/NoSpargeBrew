2e61542576710d939f62d20b3003c49e79985854
Per Rosen <exxoff@gmail.com>
2019-02-28 17:01:46 +0100

    Added parameters and help information

--- noSparge.ps1
+++ noSparge.ps1
@@ -1,22 +1,28 @@
-Function NoSpargeVol(){ 
-	#Batch size (Liters)
-	$BatchSize = 23
-	#Max Boil vol (GrainFater = 30L)
-	$MaxBoilVol = 30
-	#Grain Bill (Kg)
-	$MaltKg = 5.5
-	#Grain Absorption (Liter/Kg)
-	$GrainAbs = 1
-	#No sparge brewhouse eff 1.2 = 120% malt needed to reach original sparge OG
-	$BrewEff = 1.2
-	#Boil Off (Liters)
-	$BoilOffLiter = 3
+param(
+	[Parameter(Mandatory=$true)]
+	[int]$BatchSize,
+	
+	[Parameter(Mandatory=$true)]
+	[int]$MaxBoilVolume,
 
-	$WaterNeeded = $BatchSize + ($BatchSize - ($BatchSize - (($MaltKg * $BrewEff) * $GrainAbs) - $BoilOffLiter))
+	[Parameter(Mandatory=$true)]
+	[int]$GrainAmount,
+
+	[int]$GrainAbsorption = 1,
+
+	[int]$BoilOffVolume = 3,
+
+	[int]$BrewEfficiency=1.2
+	
+)
+
+Function NoSpargeVol(){ 
+
+	$WaterNeeded = $BatchSize + ($BatchSize - ($BatchSize - (($GrainAmount * $BrewEfficiency) * $GrainAbsorption) - $BoilOffVolume))
 	Write-Host -BackgroundColor yellow -ForegroundColor red "You will need $WaterNeeded L to reach your batch size of $BatchSize L" 
 	
 	#Max mash vol
-	$MaxMashWater = $MaxBoilVol - ($MaltKg * $BrewEff)
+	$MaxMashWater = $MaxBoilVolume - ($GrainAmount * $BrewEfficiency)
 	Write-Host -BackgroundColor yellow -ForegroundColor red "Mash vol: $MaxMashWater" 
 	#Water to be added before boil
 	$AddToBoilWater = $WaterNeeded - $MaxMashWater
@@ -24,3 +30,45 @@ Function NoSpargeVol(){
 }
 
 NoSpargeVol 
+
+
+<#
+
+.SYNOPSIS
+Powershell script for calculating water requirements for no-sparge brews.
+
+.DESCRIPTION
+This script will help brewers calculate the water required when doing no-sparge brews.
+
+.PARAMETER BatchSize
+	The volume going into fermenter (L).
+
+.PARAMETER MaxBoilVolume
+	The maximum volume for your brew kettle (L).
+
+.PARAMETER GrainAmount
+	The grain bill for your brew (kg).
+
+.PARAMETER BrewEfficiency
+	Optional.
+	To compensate for the slightly poorer efficiency in no-sparge brews the grain bill will be multiplied with this number.
+	Default is 1.2 (120% is needed to reach original OG)
+
+.PARAMETER BoilOffVolume
+	Optional.
+	The amount of water that is lost during boil (L).
+	Default is 3.
+
+.PARAMETER GrainAbsorption
+	Optional.
+	The amount of water the is absorbed by the grain (L/kg).
+	Default is 1.
+.EXAMPLE
+./noSparge.ps1 -GrainAmount 5.5 -BatchSize 23 -MaxBoilVolume 30
+
+
+
+.LINK
+https://github.com/tedelm/NoSpargeBrew
+
+#>
