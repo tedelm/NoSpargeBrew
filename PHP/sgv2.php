e80dedb6443709ccb22598b8271779d6eee746d0
tedelm <ted.elmenheim@gmail.com>
2019-04-03 21:14:42 +0200

    fixed menu

--- PHP/sgv2.php
+++ PHP/sgv2.php
@@ -50,11 +50,13 @@ if(isset($Sugars)){
 <html>
    <body>
    <h1>Specific Gravity Calculator</h1>
-   </br>
    <table class='tableClass2'>
 	 <tr>
+		<td><a href='http://softwaist.se/NoSparge/index.php'>No Sparge Calculator</a> | <a href='http://softwaist.se/NoSparge/sg.php'>Add sugars</a> | <a href='http://softwaist.se/NoSparge/sgv2.php'>Calc extract</a> | <a href='http://softwaist.se/NoSparge/sgv3.php'>Dilute wort</a> </td>
+	 </tr>    
+	 <tr>
 		<td><?php Echo $EchoResult; ?></td>
-	 </tr>
+	 </tr>	 
 	</table>
 	</br>   
 	
